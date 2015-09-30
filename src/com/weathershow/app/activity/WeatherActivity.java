4e1452f53783f403a297343e2930c2771ec4464e
Sun <sunjing199103@gmail.com>
2015-09-30 14:18:02 +0800

    新增后台自动更新天气的功能。

--- src/com/weathershow/app/activity/WeatherActivity.java
+++ src/com/weathershow/app/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.weathershow.app.activity;
 
 import com.weathershow.app.R;
+import com.weathershow.app.service.AutoUpdateService;
 import com.weathershow.app.util.HttpCallbackListener;
 import com.weathershow.app.util.HttpUtil;
 import com.weathershow.app.util.Utility;
@@ -137,6 +138,8 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent=new Intent(this,AutoUpdateService.class);
+		startService(intent);
 	}
 	@Override
 	public void onClick(View v) {
